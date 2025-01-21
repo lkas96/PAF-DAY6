05ccf4a3e8d223eb3c099d6f9c0f328134b8a4c9
lkas96 <kwek_lawson@hotmail.com>
2025-01-21 11:27:36 +0800

    Initial Template

---
+++ src/main/java/vttp/b5/paf/day6/Day6/Day6Application.java
@@ -0,0 +1,13 @@
+package vttp.b5.paf.day6.Day6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day6Application.class, args);
+	}
+
+}
---
+++ src/test/java/vttp/b5/paf/day6/Day6/Day6ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.b5.paf.day6.Day6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
